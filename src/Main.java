2444b6404cbf31c6a3bfa2f1698fafc979e8bef0
mujanajinsoy-jajoy <david.mujunajinsoy@correounivalle.edu.co>
2024-03-29 22:43:04 -0500

    cambio en el ingreso de valor de ahorro

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,10 @@ private static List<Cliente> clientes = new ArrayList<>();
             System.out.println("Seleccione una opción:");
             System.out.println("1. Insertar cliente");;
             System.out.println("2. Eliminar cliente");
-            System.out.println("3. Buscar cliente por nombre y ver su ahorro");
-            System.out.println("4. Listar todos los clientes");
-            System.out.println("5. Salir");
+            System.out.println("3. Actualizar dinero ahorrado");
+            System.out.println("4. Buscar cliente por nombre y ver su ahorro");
+            System.out.println("5. Listar todos los clientes");
+            System.out.println("6. Salir");
 
             int opcion = scanner.nextInt();
             scanner.nextLine(); // Limpiar el buffer del scanner
@@ -27,12 +28,14 @@ private static List<Cliente> clientes = new ArrayList<>();
                     eliminarCliente();
                     break;
                 case 3:
+                    actualizarAhorroCliente();
+                case 4:
                     buscarClientePorNombre();
                     break;
-                case 4:
+                case 5:
                     listarClientes();
                     break;
-                case 5:
+                case 6:
                     continuar = false;
                     break;
                 default:
@@ -48,51 +51,57 @@ private static List<Cliente> clientes = new ArrayList<>();
         System.out.println("Ingrese la cédula del cliente:");
         String cedula = scanner.nextLine();
         int opcion;
-    String nivelingresos = ""; // Declarar la variable fuera del switch
+        String nivelingresos = ""; // Declarar la variable fuera del switch
 
-    do {
-        System.out.println("Seleccione el nivel de ingresos del cliente:");
-        System.out.println("1. Bajo");
-        System.out.println("2. Medio");
-        System.out.println("3. Alto");
-        opcion = scanner.nextInt();
+        do {
+            System.out.println("Seleccione el nivel de ingresos del cliente:");
+            System.out.println("1. Bajo");
+            System.out.println("2. Medio");
+            System.out.println("3. Alto");
+            opcion = scanner.nextInt();
         
-        // Limpiar el buffer del scanner
-        scanner.nextLine();
+            // Limpiar el buffer del scanner
+            scanner.nextLine();
 
-        switch (opcion) {
-            case 1:
-                nivelingresos = "Bajo";
-                System.out.println("El cliente tiene ingresos bajos.");
-                break;
-            case 2:
-                nivelingresos = "Medio";
-                System.out.println("El cliente tiene ingresos medios.");
-                break;
-            case 3:
-                nivelingresos = "Alto";
-                System.out.println("El cliente tiene ingresos altos.");
-                break;
-            default:
-                System.out.println("Opción inválida. Por favor, seleccione una opción válida (1, 2 o 3).");
-                break;
-        }
-    } while (nivelingresos.isEmpty()); // Continuar el bucle si el nivel de ingresos no ha sido asignado
+            switch (opcion) {
+                case 1:
+                    nivelingresos = "Bajo";
+                    System.out.println("El cliente tiene ingresos bajos.");
+                    break;
+                case 2:
+                    nivelingresos = "Medio";
+                    System.out.println("El cliente tiene ingresos medios.");
+                    break;
+                case 3:
+                    nivelingresos = "Alto";
+                    System.out.println("El cliente tiene ingresos altos.");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Por favor, seleccione una opción válida (1, 2 o 3).");
+                    break;
+            }
+        } while (nivelingresos.isEmpty()); // Continuar el bucle si el nivel de ingresos no ha sido asignado
 
        
-    scanner.nextLine(); // Limpiar el buffer del scanner
+        scanner.nextLine(); // Limpiar el buffer del scanner
         System.out.println("Ingrese la fecha de creación del cliente:");
         String fechacreacion = scanner.nextLine();
-
-        System.out.println("Ingrese la cantidad de dinero ahorrado:");
-        Double ahorro  = scanner.nextDouble();
+        
+        double ahorro =0;
+        while(ahorro <=0){//ingresar un valor de ahorro mayor que cero
+            System.out.println("Ingrese la cantidad de dinero ahorrado:");
+            ahorro  = scanner.nextDouble();
+            if(ahorro<=0){
+                    System.out.println("ingrese un valor correcto, mayor o igual que 0$");
+            }
+        }
 
         Cliente cliente = new Cliente(nombre, cedula, ahorro ,nivelingresos ,fechacreacion);
         clientes.add(cliente);
         System.out.println("Cliente insertado correctamente.");
     }
 
-    /*private static void actualizarAhorroCliente() {
+    private static void actualizarAhorroCliente() {
         System.out.println("Ingrese el nombre del cliente:");
         String nombre = scanner.nextLine();
         Cliente clienteEncontrado = null;
@@ -110,7 +119,7 @@ private static List<Cliente> clientes = new ArrayList<>();
         } else {
             System.out.println("Cliente no encontrado.");
         }
-    }*/
+    }
 
     private static void eliminarCliente() {
         System.out.println("Ingrese el nombre del cliente:");
